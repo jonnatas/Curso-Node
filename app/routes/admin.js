3d0728984e279f1deb40cf0e21cdd22bd012370e
jonnatas <jonatas_lenon@hotmail.com>
2017-02-13 16:19:48 -0200

    add express validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -5,9 +5,21 @@ module.exports = function(application){
 	});
 
 	application.post('/noticias/salvar', function(req, res){
-		//res.send("Chegou napágina");
 		var noticia = req.body;
-/*		res.send(noticia);*/
+			
+		req.assert('titulo','Titulo é obrigatório').notEmpty();
+		req.assert('resumo','Resumo é obtigatório').notEmpty();
+		req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10,100);
+		req.assert('autor','Autor é obtigatório').notEmpty();
+		req.assert('data_noticia','Data é obtigatório').notEmpty().isDate({format : 'YYYY-MM-DD'});
+		req.assert('noticia','Noticia é obtigatório').notEmpty;
+
+		var erros = req.validationErrors();
+
+		if (erros) {
+			res.render("admin/form_add_noticia");
+			return;
+		}
 
 		var connection = application.config.dbConnection();
 		var NoticiasDAO = new application.app.models.NoticiasDAO(connection);
--- config/server.js
+++ config/server.js
@@ -3,12 +3,13 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true})); 
-
+app.use(expressValidator());
 
 consign()
 	.include('./app/routes')
