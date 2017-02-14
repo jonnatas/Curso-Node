190ec164fcb63277ab2933b6cc84c0859b26089d
jonnatas <jonatas_lenon@hotmail.com>
2017-02-14 02:58:24 -0200

    add info off index

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,4 +1,10 @@
 module.exports.index = function (application, request, response) {
-	response.render("home/index")
-	// body...
+	var connection = application.config.dbConnection();
+	var noticiasModel = new application.app.models.NoticiasDAO(connection);
+
+	noticiasModel.get5UltimasNoticias(function(erro, result){
+		console.log(result);
+		response.render("home/index")
+	});
+	
 }
